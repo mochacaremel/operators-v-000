62e07e04538d2c43289b07a16a80c30cb6a68bcd
Asheley McDonald <asheleymcdonald@gmail.com>
2018-04-05 15:05:14 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,16 +1,13 @@
-# def unsafe?(speed)
-# if  speed < 40 
-#   puts unsafe 
-#   else puts not_safe
-# end
-
 def unsafe?(speed)
-  if speed < 40 then
-  unsafe(0, 20) + "..."
-else 
-  not_safe
+	if speed > 60 || speed < 40
+		return true;
+	else
+		return false;
+	end
 end
 
-# def not_safe?(speed)
-# 	speed > 60 ? "not_safe" : "safe"
-# end
+
+
+def not_safe?(speed)
+	speed > 60 || speed < 40 ? true : false;
+end
