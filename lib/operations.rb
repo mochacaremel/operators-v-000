abac4ed1d226ee272ead92bd0edd3e4e431de1ca
Asheley McDonald <asheleymcdonald@gmail.com>
2018-04-05 14:53:33 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -4,6 +4,13 @@
 #   elsif puts "not_safe"
 # end
 
+def unsafe?(speed)
+if  speed < 40 ? unsafe : not_safe
+  
+else
+  something_else
+end
+
 def not_safe?(speed)
 # 	speed > 60 ? "not_safe" : "safe"
 result = speed ? safe : not_safe
